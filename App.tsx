3cc8f3711491883f3ded8cfb21ffb2cb8cd32bef
Adam Naoui <adam.naoui@polymtl.ca>
2022-09-27 21:39:43 -0400

    Lint Correction

--- App.tsx
+++ App.tsx
@@ -1,48 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import Login from './views/login/Login';
-import { VendorContext } from './context/Vendor';
-import SignUp from './views/sign_up/SignUp';
-import {useTranslation} from 'react-i18next';
+import React from "react";
+import { StyleSheet } from "react-native";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { VendorContext } from "./context/Vendor";
+import SignUp from "./views/sign_up/SignUp";
+import { useTranslation } from "react-i18next";
 
 export default function App() {
-  const {t, i18n} = useTranslation('translation')
+  const { t, i18n } = useTranslation("translation");
   const [language, setLanguage] = React.useState(i18n.language);
   const [storeId, setStoreId] = React.useState<string>("");
-  const storeIdContext = {storeId, setStoreId};
+  const storeIdContext = { storeId, setStoreId };
   const authLink = setContext((_, { headers }) => {
     return {
       headers: {
         ...headers,
-        authorization:`{"storeId":"` + storeId + `"}`
-      }
-    }
+        authorization: `{"storeId":"` + storeId + `"}`,
+      },
+    };
   });
   const httpLink = createHttpLink({
-    uri: 'http://localhost:4000/graphql',
+    uri: "http://localhost:4000/graphql",
   });
   const client = new ApolloClient({
     link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
   });
-  const handleChange = (event:any) => {
+  const handleChange = (event: any) => {
     setLanguage(event.target.value as string);
-    i18n.changeLanguage(event.target.value)
-};
+    i18n.changeLanguage(event.target.value).then(() => {
+      console.log("Language changed to " + event.target.value);
+    });
+  };
   return (
     <VendorContext.Provider value={storeIdContext}>
       <ApolloProvider client={client}>
-        <SignUp/>
+        <SignUp />
       </ApolloProvider>
     </VendorContext.Provider>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#FFA500',
+    backgroundColor: "#FFA500",
   },
 });
--- views/sign_up/SignUp.tsx
+++ views/sign_up/SignUp.tsx
@@ -1,59 +1,88 @@
-import React from "react"
-import { Keyboard, KeyboardTypeOptions, StyleSheet, View } from "react-native"
-import { Button, Text, TextInput, Card, HelperText} from 'react-native-paper'
-import {signUpStyles} from "./SignUpStyles";
-import {fieldsArray} from "./SignUpFields";
-import {initialSignUpCredentialsState} from "./reducers/SignUpCredentialsReducerState";
-import {signUpCredentialsReducer} from "./reducers/SignUpCredentialsReducer";
-import { SignUpCredentialsReducerActions } from "./reducers/SignUpCredentialsReducerActions";
-import { AccountInput, SignUpErrorMessage } from "../../interfaces/SignUpInterfaces";
-import { useTranslation } from "react-i18next";
-
+import React from 'react';
+import {KeyboardTypeOptions, View} from 'react-native';
+import {Button, Card, HelperText, Text, TextInput} from 'react-native-paper';
+import {signUpStyles} from './SignUpStyles';
+import {fieldsArray} from './SignUpFields';
+import {initialSignUpCredentialsState} from './reducers/SignUpCredentialsReducerState';
+import {signUpCredentialsReducer} from './reducers/SignUpCredentialsReducer';
+import {SignUpCredentialsReducerActions} from './reducers/SignUpCredentialsReducerActions';
+import {
+  AccountInput,
+  SignUpErrorMessage,
+} from '../../interfaces/SignUpInterfaces';
+import {useTranslation} from 'react-i18next';
 
 const SignUp = () => {
-    const {t: translation} = useTranslation('translation')
-    const [{verifyPassword, accountInput, signUpErrorMessage}, dispatchCredentialsState]
-    = React.useReducer(signUpCredentialsReducer, initialSignUpCredentialsState);
-    const errorExists = (attribut: string) => {
-        return signUpErrorMessage[attribut + 'Error' as keyof SignUpErrorMessage] === undefined ? false : signUpErrorMessage[attribut + 'Error' as keyof SignUpErrorMessage].length > 0;
-    }
-    return ( 
-        <View style={signUpStyles.root}>
-            <View style={signUpStyles.signUp}>
-                <Text>Sign Up</Text>
-            </View>
-            <Card style={signUpStyles.card}>
-                <View style={signUpStyles.fieldsView}>
-                    {fieldsArray.map((field, index) => {
-                        return (
-                            <View style={signUpStyles.fieldView}>
-                                <Text>{field.label}</Text>
-                                <TextInput
-                                    style={{height: 40}}
-                                    key={index}
-                                    value={field.attribut === 'confirmPassword' ? verifyPassword : accountInput[field.attribut as keyof AccountInput]}
-                                    keyboardType={field.keyboardType as KeyboardTypeOptions}
-                                    secureTextEntry={field.secure}
-                                    onChangeText={(text) => {dispatchCredentialsState(field.onChange(text) as SignUpCredentialsReducerActions)}}
-                                    mode="outlined"
-                                    error={false}
-                                />
-                                <HelperText style={{
-                                    height: errorExists(field.attribut) ? 'auto' : 0,
-                                }} padding='none' type="error">
-                                    {translation(signUpErrorMessage[field.attribut + 'Error' as keyof SignUpErrorMessage])}
-                                </HelperText>
-                            </View>
-                        )
-                    })}
-                    </View>
-                    <View style={signUpStyles.buttonView}>
-                        <Button style={signUpStyles.button} mode='contained' onPress={() => {dispatchCredentialsState({type :'CHECK_SIGN_UP_CREDENTIALS'})}}>Sign Up</Button>
-                    </View>
-                
-            </Card>
+  const {t: translation} = useTranslation('translation');
+  const [
+    {verifyPassword, accountInput, signUpErrorMessage},
+    dispatchCredentialsState,
+  ] = React.useReducer(signUpCredentialsReducer, initialSignUpCredentialsState);
+  const errorExists = (attribute: string) => {
+    return signUpErrorMessage[
+      (attribute + 'Error') as keyof SignUpErrorMessage
+    ] === undefined
+      ? false
+      : signUpErrorMessage[(attribute + 'Error') as keyof SignUpErrorMessage]
+          .length > 0;
+  };
+  return (
+    <View style={signUpStyles.root}>
+      <View style={signUpStyles.signUp}>
+        <Text>Sign Up</Text>
+      </View>
+      <Card style={signUpStyles.card}>
+        <View style={signUpStyles.fieldsView}>
+          {fieldsArray.map(field => {
+            return (
+              <View style={signUpStyles.fieldView} key={field.attribute}>
+                <Text>{field.label}</Text>
+                <TextInput
+                  style={{height: 40}}
+                  value={
+                    field.attribute === 'confirmPassword'
+                      ? verifyPassword
+                      : accountInput[field.attribute as keyof AccountInput]
+                  }
+                  keyboardType={field.keyboardType as KeyboardTypeOptions}
+                  secureTextEntry={field.secure}
+                  onChangeText={text => {
+                    dispatchCredentialsState(
+                      field.onChange(text) as SignUpCredentialsReducerActions,
+                    );
+                  }}
+                  mode="outlined"
+                  error={false}
+                />
+                <HelperText
+                  style={{
+                    height: errorExists(field.attribute) ? 'auto' : 0,
+                  }}
+                  padding="none"
+                  type="error">
+                  {translation(
+                    signUpErrorMessage[
+                      (field.attribute + 'Error') as keyof SignUpErrorMessage
+                    ],
+                  )}
+                </HelperText>
+              </View>
+            );
+          })}
         </View>
-    )
-}
+        <View style={signUpStyles.buttonView}>
+          <Button
+            style={signUpStyles.button}
+            mode="contained"
+            onPress={() => {
+              dispatchCredentialsState({type: 'CHECK_SIGN_UP_CREDENTIALS'});
+            }}>
+            Sign Up
+          </Button>
+        </View>
+      </Card>
+    </View>
+  );
+};
 
-export default SignUp
+export default SignUp;
